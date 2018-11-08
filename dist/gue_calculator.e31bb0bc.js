01fff1b718dd1e110c6fbe7c94b7a3ed2afbb6b2
Per Lundkvist <persyl@gmail.com>
2018-11-08 10:50:18 +0100

    Updates

--- dist/gue_calculator.e31bb0bc.js
+++ dist/gue_calculator.e31bb0bc.js
@@ -1406,7 +1406,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "56176" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "54192" + '/');
 
   ws.onmessage = function (event) {
     var data = JSON.parse(event.data);
